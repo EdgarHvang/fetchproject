e6f71e119ba1fe027cc63fb76056fe15070b6e12
EdgarHvang <Edgarhvang@gmail.com>
2024-07-29 13:34:39 -0500

    first commit

---
+++ FetchProject/FetchProject/FetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectApp.swift
+//  FetchProject
+//
+//  Created by Qiyao Huang on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
